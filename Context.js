ccc25444d34b83e183e2cc1abdb573ccb4c12539
Queen Mavis <divinewildflower47@gmail.com>
2026-09-03 13:34:03 -0500

    Add Context.js with actual integration code

---
+++ Context.js
@@ -0,0 +1,10 @@
+/**
+ * CONTEXT TAB
+ * Copy this entire code into your scenario's CONTEXT tab
+ */
+
+WorldWeaver("context");
+const modifier = (text) => {
+  return { text, stop };
+};
+modifier(text);
